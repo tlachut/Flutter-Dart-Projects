50e003b1f383621e2d5408780ff1cb86a877ad71
Tomasz Łachut <tomus.lachut@gmail.com>
2025-07-23 18:02:04 +0200

    Project 5 Initial commit: created new project and set up fundaments

---
+++ 005_expense_tracker_app/README.md
@@ -0,0 +1,13 @@
+# Expense Tracker App
+
+This Expense Tracker App is the fifth project I built while following the fifth chapter of the [Flutter &amp; Dart - The Complete Guide [2025 Edition]](https://www.udemy.com/course/learn-flutter-dart-to-build-ios-android-apps/) Udemy course.
+
+<p align="center">
+  <img src="" alt="" width="300"/>
+  <img src="" alt="" width="300"/>
+  <img src="" alt="" width="300"/>
+</p>
+
+---
+
+### What I learned during this chapter:
---
+++ 005_expense_tracker_app/lib/expense.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Expense extends StatefulWidget {
+  const Expense({super.key});
+
+  @override
+  State<Expense> createState() {
+    return _ExpenseState();
+  }
+}
+
+class _ExpenseState extends State<Expense> {
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        children: [
+          Text('The chart'),
+          Text('Expenses list...'),
+        ],
+      ),
+    );
+  }
+}
