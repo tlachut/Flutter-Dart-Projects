9bdcdf503f3b7d8b4c3f6ba42853b4f0246d323a
Tomasz Łachut <tomus.lachut@gmail.com>
2025-07-26 17:52:31 +0200

    Setting & Using a Color Scheme, Setting Text Themes

--- 005_expense_tracker_app/lib/main.dart
+++ 005_expense_tracker_app/lib/main.dart
@@ -1,10 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: Colors.deepPurpleAccent,
+);
 
 void main() {
   runApp(
-    const MaterialApp(
-      home: Expenses(),
+    MaterialApp(
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Colors.deepPurpleAccent,
+          foregroundColor: Colors.white,
+        ),
+        cardTheme: const CardThemeData().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            vertical: 8,
+            horizontal: 16,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: GoogleFonts.lato(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+      home: const Expenses(),
     ),
   );
 }
