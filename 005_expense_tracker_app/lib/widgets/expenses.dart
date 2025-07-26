60f047661df61bb5a395135521c55cb77f09cfc4
Tomasz Łachut <tomus.lachut@gmail.com>
2025-07-26 17:03:14 +0200

    Displaying proper info when expenses list is empty, Showing & Managing Snackbars

--- 005_expense_tracker_app/lib/widgets/expenses.dart
+++ 005_expense_tracker_app/lib/widgets/expenses.dart
@@ -43,13 +43,44 @@ class _ExpensesState extends State<Expenses> {
   }
 
   void _removeExpense(Expense expense) {
+    final expenseIndex = _expensesList.indexOf(expense);
+
     setState(() {
       _expensesList.remove(expense);
     });
+
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(
+      SnackBar(
+        duration: const Duration(seconds: 3),
+        content: const Text('Deleted expense.'),
+        action: SnackBarAction(
+          label: 'Undo',
+          onPressed: () {
+            setState(() {
+              _expensesList.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text('No expenses found. Start adding some!'),
+    );
+
+    if (_expensesList.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _expensesList,
+        onRemoveExpense: _removeExpense,
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Expense Tracker'),
@@ -64,10 +95,7 @@ class _ExpensesState extends State<Expenses> {
         children: [
           const Text('The chart'),
           Expanded(
-            child: ExpensesList(
-              expenses: _expensesList,
-              onRemoveExpense: _removeExpense,
-            ),
+            child: mainContent,
           ),
         ],
       ),
