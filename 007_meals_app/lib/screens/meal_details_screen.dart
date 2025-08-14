9c5c965a6c045a25f8acd1400bff91b13fc5aa44
Tomasz Łachut <tomus.lachut@gmail.com>
2025-08-14 17:53:31 +0200

    Refactor MealDetailsScreen to display ingredients and steps in a scrollable view

--- 007_meals_app/lib/screens/meal_details_screen.dart
+++ 007_meals_app/lib/screens/meal_details_screen.dart
@@ -12,11 +12,56 @@ class MealDetailsScreen extends StatelessWidget {
       appBar: AppBar(
         title: Text(meal.title),
       ),
-      body: Column(
-        children: [
-          Image.network(meal.imageUrl),
-        ],
-      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Image.network(
+                meal.imageUrl,
+                height: 300,
+                width: double.infinity,
+                fit: BoxFit.cover,
+              ),
+              const SizedBox(height: 12),
+              Text(
+                'Ingredients:',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+              const SizedBox(height: 8),
+              for (final ingredient in meal.ingredients)
+                Text(
+                  '- $ingredient',
+                  style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onSurface,
+                  ),
+                ),
+              const SizedBox(height: 12),
+              Text(
+                'Steps:',
+                style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+              const SizedBox(height: 8),
+              for (final step in meal.steps.asMap().entries)
+                Padding(
+                  padding: const EdgeInsets.all(4.0),
+                  child: Text(
+                    '${step.key + 1}. ${step.value}',
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                      color: Theme.of(context).colorScheme.onSurface,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+            ],
+          ),
+        ),
+      ), // Image.network
     );
   }
 }
